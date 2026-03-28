3367a50005fddf9645b5cb3c26af710b72ec5286
FreeformWave544 <165945352+FreeformWave544@users.noreply.github.com>
2026-03-28 10:30:11 +0000

    Main Menu Background

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -5,3 +5,14 @@ func _on_start_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	add_child(load("res://Scenes/menus/options_menu/master_options_menu_with_tabs.tscn").instantiate())
+
+func _ready() -> void:
+	$Panel/Sprite2D.texture.noise.cellular_jitter = randi_range(1.000, 10.000)
+
+var change = 0.1
+func _physics_process(_delta: float) -> void:
+	$Panel/Sprite2D.texture.noise.cellular_jitter += change
+	if $Panel/Sprite2D.texture.noise.cellular_jitter >= 50:
+		change *= -1
+	elif $Panel/Sprite2D.texture.noise.cellular_jitter < 0.0:
+		change *= -1
